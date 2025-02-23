8fb48eefda60fd3193a1d377ea969f5f4ed0f73f
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-02-23 10:04:59 +0530

    initial setup done

---
+++ hardhat.config.ts
@@ -0,0 +1,52 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Make sure to keep your private key secure
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0000000000000000000000000000000000000000000000000000000000000000";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    sonic: {
+      url: "https://rpc.soniclabs.com",
+      chainId: 146,
+      accounts: [PRIVATE_KEY],
+    },
+    sonicTestnet: {
+      url: "https://rpc.blaze.soniclabs.com",
+      chainId: 57054,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sonic: process.env.SONICSCAN_API_KEY || "",
+      sonicTestnet: process.env.SONICSCAN_TESTNET_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "sonic",
+        chainId: 146,
+        urls: {
+          apiURL: "https://api.sonicscan.org/api",
+          browserURL: "https://sonicscan.org",
+        },
+      },
+      {
+        network: "sonicTestnet",
+        chainId: 57054,
+        urls: {
+          apiURL: "https://api-testnet.sonicscan.org/api",
+          browserURL: "https://testnet.sonicscan.org",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
